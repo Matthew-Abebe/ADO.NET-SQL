6bdcf03c0f01dff0e5beab12d4d43336e54ff404
Matthew Abebe <matthewabebe.inbox@gmail.com>
2019-08-02 13:49:29 -0500

    first commit

---
+++ DepartmentEmployees/Models/Department.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DepartmentsEmployees.Models
+{
+    // C# representation of the Department table
+    public class Department
+    {
+        public int Id { get; set; }
+        public string DeptName { get; set; }
+    }
+}
